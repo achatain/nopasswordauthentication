37b39dccfdc2d11af056823e6fdfe9b4b8d1c21f
achatain <achatain@users.noreply.github.com>
2016-03-25 23:43:11 +0000

    Added license information in headers of source files

--- src/main/java/com/github/achatain/nopasswordauthentication/notification/email/SendGridEmailServiceImpl.java
+++ src/main/java/com/github/achatain/nopasswordauthentication/notification/email/SendGridEmailServiceImpl.java
@@ -1,3 +1,22 @@
+/*
+ * https://github.com/achatain/nopasswordauthentication
+ *
+ * Copyright (C) 2016 Antoine R. "achatain" (achatain [at] outlook [dot] com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.achatain.nopasswordauthentication.notification.email;
 
 import com.github.achatain.nopasswordauthentication.exception.InternalServerException;
@@ -8,7 +27,7 @@ import com.sendgrid.SendGridException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SendGridEmailServiceImpl implements EmailService {
+class SendGridEmailServiceImpl implements EmailService {
 
     private static final Logger LOG = Logger.getLogger(SendGridEmailServiceImpl.class.getName());
 
